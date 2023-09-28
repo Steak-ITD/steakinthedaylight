ba994f32c4c7efd74460150a296b1a30d64ca554
Steak-ITD <skye.lewis@proton.me>
2023-09-27 20:49:01 -0400

    comment section on all writing zine and LSP pages

---
+++ Asset/comment.js
@@ -0,0 +1,11 @@
+var html= `<div id="cusdis_thread"
+data-host="https://cusdis.com"
+data-app-id="1eb7e221-ec67-4d9d-bd36-1139ea9b02a4"
+data-page-id="{{ PAGE_ID }}"
+data-page-url="{{ PAGE_URL }}"
+data-page-title="{{ PAGE_TITLE }}"
+></div>
+<script async defer src="https://cusdis.com/js/cusdis.es.js"></script>
+`;
+
+document.write(html);
